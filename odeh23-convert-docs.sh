035f2e8bca3f1411bd7fe55a7885c87146629749
odeh23 <dannyo23@live.unc.edu>
2017-07-24 16:01:30 -0400

    Update odeh23-convert-docs.sh

--- odeh23-convert-docs.sh
+++ odeh23-convert-docs.sh
@@ -4,12 +4,14 @@
 INPUTFILE=spitbars.md
 # Convert markdown to HTML
 pandoc -o spitbars.html $INPUTFILE
+# Convert to latex
+pandoc -o spitbars.tex $INPUTFILE
 # Convert markdown to DOCX
 pandoc -o spitbars.docx $INPUTFILE
 # Convert markdown to ODT
 pandoc -o spitbars.odt $INPUTFILE
 # Convert markdown to PDF
-pandoc -o spitbars.pdf $INPUTFILE
-pandoc $INPUTFILE --latex-engine=pdflatex -o spitbars.pdf
+#pandoc -o spitbars.pdf $INPUTFILE
+#pandoc $INPUTFILE --latex-engine=pdflatex -o spitbars.pdf
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
 echo "almond milk"
